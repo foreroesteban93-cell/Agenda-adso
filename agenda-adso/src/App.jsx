7e1092e14af9ac3df649e85855246de9d7070210
foreroesteban93-cell <foreroesteban93@gmail.com>
2026-02-17 07:47:11 -0500

    Actualizacion de la clase pasada

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,1 +1,48 @@
-export default function App() { const fecha = new Date().toLocaleString(); return ( <main> <h1>Hola, soy Esteban - Aprendiz ADSO</h1><p>me gustaria profundizar mis conocimientos en el desarrollo web</p></main>)}
+import { useState } from "react";
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+import FormularioContacto from "./components/FormularioContacto";
+
+export default function App() {
+  const [contactos, setContactos] = useState([
+    {
+      id: 1,
+      nombre: "Carolina Pérez",
+      telefono: "300 123 4567",
+      correo: "carolina@sena.edu.co",
+      etiqueta: "Compañera",
+    },
+  ]);
+
+  // Agregar
+  const agregarContacto = (nuevo) => {
+    setContactos((prev) => [...prev, { id: Date.now(), ...nuevo }]);
+  };
+
+  // Eliminar
+  const eliminarContacto = (id) => {
+    setContactos((prev) => prev.filter((c) => c.id !== id));
+  };
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO v2</h1>
+
+      <FormularioContacto onAgregar={agregarContacto} />
+
+      <section className="lista-contactos">
+        {contactos.map((c) => (
+          <ContactoCard
+            key={c.id}
+            id={c.id}
+            nombre={c.nombre}
+            telefono={c.telefono}
+            correo={c.correo}
+            etiqueta={c.etiqueta}
+            onDelete={eliminarContacto}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
