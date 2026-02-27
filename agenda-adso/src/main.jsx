641053b2bcf29a3dcb7ca30246b0bc89f42574f3
foreroesteban93-cell <foreroesteban93@gmail.com>
2026-02-27 10:42:42 -0500

    Clase_8_Agenda_ADSO_v6_Validaciones_UX

--- agenda-adso/src/main.jsx
+++ agenda-adso/src/main.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
-// React: la librería principal
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Importamos el componente raíz
+import App from "./App.jsx";
+// Importante: trae Tailwind a la app
+import "./index.css";
 
-import ReactDOM from 'react-dom/client'
-// ReactDOM: dibuja React en el navegador
-
-import App from './App.jsx'
-// Importamos el componente raíz (nuestra Agenda ADSO)
-
-import './index.css'
-// Importamos los estilos globales
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Punto de entrada de la app: renderiza <App /> dentro de #root
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    {/* Aquí montamos nuestra Agenda ADSO */}
-  </React.StrictMode>,
+  </React.StrictMode>
 );
